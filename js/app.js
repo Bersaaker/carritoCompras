5cad8d4e52d309ab9d5adab8c66a5fcf190ef03d
Alejandro Avendaño <alex.aven.luna@gmail.com>
2022-09-28 00:20:45 -0500

    Actualizar la cantidad de elementos si el elemento ya existe

--- js/app.js
+++ js/app.js
@@ -36,8 +36,27 @@ function leerDatosCurso(curso){
         cantidad: 1
     }
 
-    //Agrega elementos al arreglo de carrito
-    articulosCarrito = [...articulosCarrito, infoCurso];
+    //Revisa si un elemento ya se encuentra en el carrito
+    const existe = articulosCarrito.some( curso => curso.id === infoCurso.id );
+    if(existe){
+        //Actualizamos la cantidad
+        const cursos = articulosCarrito.map( curso => {
+            if( curso.id === infoCurso.id ){
+                curso.cantidad++;
+                return curso; //retorna el objeto actualizado
+            }else{
+                return curso;//retorna los objetos que no son los duplicados
+            }
+        });
+        articulosCarrito = [...cursos];
+    } else{
+        //Agregamos el curso al carrito
+        articulosCarrito = [...articulosCarrito, infoCurso];
+    }
+
+
+
+
 
     console.log(articulosCarrito);
 
